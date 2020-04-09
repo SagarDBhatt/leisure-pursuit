cafbbae17c422d8a4327622fc1e59c26a564cfc6
Sagar Bhatt <49220046+Sagar1087@users.noreply.github.com>
2020-04-08 21:49:25 -0600

    Create AssignTwo.java

---
+++ src/main/java/com/JavaAssignments/AssignTwo.java
@@ -0,0 +1,21 @@
+package com.JavaAssignments;
+
+import javax.swing.*;
+
+public class AssignTwo {
+
+    public static void main(String[] args) {
+        int n = Integer.parseInt(JOptionPane.showInputDialog("Enter number of rows: "));
+
+        AssignTwo obj = new AssignTwo();
+        obj.star(n);
+    }
+
+    public void star(int n){
+        for(int i =1; i<= n ; i++){
+            for(int j=1; j<= i; j++){
+                System.out.println("*");
+            }
+        }
+    }
+}
