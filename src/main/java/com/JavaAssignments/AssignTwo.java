92d26191e1b38c87a243afbfc0c9ce2d2da872ae
Sagar Bhatt <49220046+Sagar1087@users.noreply.github.com>
2020-04-08 21:52:24 -0600

    Update AssignTwo.java

--- src/main/java/com/JavaAssignments/AssignTwo.java
+++ src/main/java/com/JavaAssignments/AssignTwo.java
@@ -14,8 +14,9 @@ public class AssignTwo {
     public void star(int n){
         for(int i =1; i<= n ; i++){
             for(int j=1; j<= i; j++){
-                System.out.println("*");
+                System.out.print("*");
             }
+            System.out.println("");
         }
     }
 }
