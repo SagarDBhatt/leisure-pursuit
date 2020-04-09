e58bc439a0eca3c19531467b464df6162d42c031
Sagar Bhatt <49220046+Sagar1087@users.noreply.github.com>
2020-04-08 21:25:56 -0600

    Update AssignOne.java

--- src/main/java/com/JavaAssignments/AssignOne.java
+++ src/main/java/com/JavaAssignments/AssignOne.java
@@ -1,4 +1,21 @@
 package com.JavaAssignments;
 
+import javax.swing.*;
+
 public class AssignOne {
-}
+
+    public static void main(String[] args) {
+        int n = Integer.parseInt(JOptionPane.showInputDialog("Enter the number: "));
+        AssignOne object = new AssignOne();
+        object.table(n);
+
+    }//end of main()
+
+    public void table(int n){
+
+        for(int i = 1; i <= 10; i++){
+            System.out.println(n + " * " + i + " = " + n*i);
+        }
+    }
+
+}//end of class
