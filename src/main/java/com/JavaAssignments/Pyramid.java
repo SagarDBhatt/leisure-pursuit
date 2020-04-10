4d5d0b95ce5ebff93997d32ed46b81ea5bc3ecd6
Sagar Bhatt <49220046+Sagar1087@users.noreply.github.com>
2020-04-09 22:46:10 -0600

    Update Pyramid.java

--- src/main/java/com/JavaAssignments/Pyramid.java
+++ src/main/java/com/JavaAssignments/Pyramid.java
@@ -1,18 +1,28 @@
 package com.JavaAssignments;
 
+import java.util.Scanner;
+
 public class Pyramid {
     public static void main(String[] args) {
+        Pyramid aObj = new Pyramid();
 
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of rows = ");
+        int n = sc.nextInt();
 
+        aObj.star(n);
     }
 
     public void star(int n){
-        for(int i=0;i<n;i++){
-            for(int j=1;j<= 2*i + 1;j++){
-             
-
-
+        for(int i=1;i<=n;i++){
+            for(int j = (2*(n-i) + 1)/2; j>0; j-- ){
+                System.out.print(" ");
             }
+
+            for(int j=1;j<= 2*i - 1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
         }
     }
 
