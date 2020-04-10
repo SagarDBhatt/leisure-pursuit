9790497986322c0c3fdc8b4f728bde51addb8af4
Sagar Bhatt <49220046+Sagar1087@users.noreply.github.com>
2020-04-09 21:56:55 -0600

    Create Pyramid.java

---
+++ src/main/java/com/JavaAssignments/Pyramid.java
@@ -0,0 +1,19 @@
+package com.JavaAssignments;
+
+public class Pyramid {
+    public static void main(String[] args) {
+
+
+    }
+
+    public void star(int n){
+        for(int i=0;i<n;i++){
+            for(int j=1;j<= 2*i + 1;j++){
+             
+
+
+            }
+        }
+    }
+
+}//end of class
