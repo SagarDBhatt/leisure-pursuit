9af33648c5f9a8c6802fded6d61bbb702b48d0a1
Sagar Bhatt <49220046+Sagar1087@users.noreply.github.com>
2020-04-11 13:17:10 -0600

    Update ReverseString.java
    Adding StringBuffer object and Reverser() method.

--- src/main/java/Miscellaneous/ReverseString.java
+++ src/main/java/Miscellaneous/ReverseString.java
@@ -4,13 +4,16 @@ public class ReverseString {
     public static void main(String[] args) {
         String str = new String("Elephant"), reverse;
         char[] rchar = new char[str.length()];
+        StringBuffer bufferObject = new StringBuffer(str);
 
         for(int i = str.length(); i > 0; i--){
             rchar[str.length()-i] = str.charAt(i-1);
         }
         reverse = new String(rchar);
         //reverse = rchar.toString();  == Doesnt work.
-        
+        bufferObject = bufferObject.reverse();
+
         System.out.println("Reverse String  = " + reverse);
+        System.out.println("Reverse String using buffer = " + bufferObject);
     }
 }
