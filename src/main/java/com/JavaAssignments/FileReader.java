8492e1ddb32ee307675c4b32ee3f6f8504af2774
Sagar Bhatt <49220046+Sagar1087@users.noreply.github.com>
2020-04-12 22:47:11 -0600

    Create FileReader.java

---
+++ src/main/java/com/JavaAssignments/FileReader.java
@@ -0,0 +1,17 @@
+package com.JavaAssignments;
+
+import java.io.File;
+
+public class FileReader {
+    public static void main(String[] args) {
+
+
+
+    }
+
+    public void read(){
+
+        File file = new File("C:/Personal Projects/leisure Pursuit/leisure-pursuit/src/main/java/ReaderFile");
+
+    }
+}
