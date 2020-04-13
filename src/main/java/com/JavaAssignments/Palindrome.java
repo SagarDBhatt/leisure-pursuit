87661c97b27fac80ac95751887c9607cdcfb6d95
Sagar Bhatt <49220046+Sagar1087@users.noreply.github.com>
2020-04-13 00:19:07 -0600

    Update Palindrome.java

--- src/main/java/com/JavaAssignments/Palindrome.java
+++ src/main/java/com/JavaAssignments/Palindrome.java
@@ -1,15 +1,25 @@
 package com.JavaAssignments;
 
+import java.util.Scanner;
+
 public class Palindrome {
     public static void main(String[] args) {
-        Palindrome obj = new 
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Insert a String = ");
+        String str = sc.nextLine();
+
+        Palindrome obj = new Palindrome();
+        obj.isPalindrome(str);
     }
 
     public void isPalindrome(String s){
         char[] reverse = new char[s.length()];
-
-        for(int index = s.length(); index > 0; index--){
-            reverse[index] = s.charAt(s.length()-index);
+/*
+        for(int index = s.length()-1; index >= 0; index--){
+            reverse[index] = s.charAt(s.length()-index-1);
+        }*/
+        for(int index = 0; index < s.length(); index++){
+            reverse[index] = s.charAt(s.length()-index-1);
         }
         String rev = new String(reverse);
 
