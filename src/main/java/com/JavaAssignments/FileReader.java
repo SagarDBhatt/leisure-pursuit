73f2c72a3c78155656bdbcd956b2a8dfcc1e4825
Sagar Bhatt <49220046+Sagar1087@users.noreply.github.com>
2020-04-12 22:51:40 -0600

    Update FileReader.java

--- src/main/java/com/JavaAssignments/FileReader.java
+++ src/main/java/com/JavaAssignments/FileReader.java
@@ -1,17 +1,23 @@
 package com.JavaAssignments;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 
-public class FileReader {
+class FileRead {
     public static void main(String[] args) {
 
 
 
     }
 
-    public void read(){
+    public void read() throws FileNotFoundException {
 
         File file = new File("C:/Personal Projects/leisure Pursuit/leisure-pursuit/src/main/java/ReaderFile");
+        FileReader fr = new FileReader(file);
+        BufferedReader br = new BufferedReader(fr);
 
+        
     }
 }
